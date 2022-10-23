b179457d25e54cb277da173be33951d8cc4f03c3
cheekynandos3600 <116491466+cheekynandos3600@users.noreply.github.com>
2022-10-23 18:53:20 +0800

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,27 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 800
+HEIGHT = 800
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (140, 120, 255)
+LINE_COLOR = (99, 85, 181)
+CIRC_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
